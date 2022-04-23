c47710fd539ae337a61ec65eaa8a2f21121bfcbc
Stanislav Ivashchenko <isn-88@ya.ru>
2022-04-23 12:51:09 +0700

    notification-service with RabbitMQ

--- notification-service/src/main/java/com/javastart/notification/NotificationApplication.java
+++ notification-service/src/main/java/com/javastart/notification/NotificationApplication.java
@@ -1,7 +1,14 @@
 package com.javastart.notification;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient// Для того чтобы наш сервис регистрировался в Eureka
 public class NotificationApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(NotificationApplication.class, args);
     }
 }
